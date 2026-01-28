6d62f1dd9c4d08e01a4c61a6007b4c12ff7ce9d0
torontotokyo <102730873+maxtvink666@users.noreply.github.com>
2026-01-28 14:31:40 +0200

    Add files via upload

---
+++ goit-js-hw-06/js/task-2.js
@@ -0,0 +1,28 @@
+class Storage 
+{
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(item) {
+    this.#items.push(item);
+  }
+  removeItem(item) {
+    this.#items.splice(this.#items.indexOf(item), this.#items.indexOf(item));
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
